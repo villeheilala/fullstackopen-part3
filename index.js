6b36f73012a8b654608a5b4f6ee3f28797b4a47b
Ville Heilala <ville@heila.la>
2018-10-29 13:30:31 +0200

    Complete 3.2-4

--- index.js
+++ index.js
@@ -32,6 +32,29 @@ app.get('/api/persons', (request, response) => {
 	response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	const person = persons.find(person => person.id === id)
+
+	if ( person ) {
+		response.json(person)
+	} else {
+		response.status(404).end()
+	}
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	persons = persons.filter(person => person.id !== id)
+
+	response.status(204).end()
+})
+
+app.get('/info', (request, response) => {
+	response.send(`<p>Database contains information about ${persons.length} persons</p>
+		<p>${new Date()}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
